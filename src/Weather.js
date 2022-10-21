92d2eee4b836232ec79752314759dd81e2d5762f
Nikavi <104579294+Nikavi@users.noreply.github.com>
2022-10-21 20:13:47 +0200

    Use the correct icon url

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloud.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       description: response.data.weather[0].description,
     });
   }
