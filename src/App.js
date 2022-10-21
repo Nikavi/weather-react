309623ed991dc1944abe0940ab4dccc705394411
Nikavi <104579294+Nikavi@users.noreply.github.com>
2022-10-21 11:04:11 +0200

    Add  links of GitHub and Netlify

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
-import Weather from './Weather';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Weather />
       </header>
+      <footer>
+        <small class="link text-center">
+          This project was coded by
+          <a
+            href="https://www.linkedin.com/in/veronika-kulikova-455b47243/"
+            rel="noopener"
+          >
+            {" "}
+            Veronika Kulikova{" "}
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/Nikavi/My-weather-project" rel="noopener">
+            open-sourced on GitHub{" "}
+          </a>{" "}
+          and{" "}
+          <a href="https://mellow-clafoutis-bd26f4.netlify.app/" rel="noopener">
+            hosted on Netlify
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
-
-export default App;
